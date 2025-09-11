a7383c3c9f5a504f74418bf2bce46ba9d51d495d
Ranjeet2610 <ranjeetsuneja26@gmail.com>
2025-09-11 21:29:12 +0530

    changes

--- puppeteer_revolt.js
+++ puppeteer_revolt.js
@@ -1501,9 +1501,9 @@ async function start_everything(IDENTIFIER_USER, IS_HEADLESS = true, START_IMMED
 		addLog({ type: "DebugMessage", message: "Trying to start bot dashboard server" });
 
 		server.listen(port, () => {
-			console.log(`Now listening to: http://localhost:${port}`);
-			open(`http://localhost:${port}`);
-			addLog({ type: "DebugMessage", message: `Now listening to: http://localhost:${port}` });
+			console.log(`Now listening to: http://35.154.130.122:${port}`);
+			open(`http://35.154.130.122:${port}`);
+			addLog({ type: "DebugMessage", message: `Now listening to: http://35.154.130.122:${port}` });
 		});
 	} catch (error) {
 		if (error.code == "ERR_SERVER_ALREADY_LISTEN") {
@@ -1529,7 +1529,7 @@ async function start_everything(IDENTIFIER_USER, IS_HEADLESS = true, START_IMMED
 
 	// 	if (key.name === "u") {
 	// 		console.log(`--------------------------`);
-	// 		console.log(`http://localhost:${port}`);
+	// 		console.log(`http://35.154.130.122:${port}`);
 	// 		console.log(`--------------------------`);
 	// 	}
 	// });
@@ -1676,8 +1676,8 @@ global_app.post("/api/add_server", async (req, res) => {
 });
 
 global_server.listen(port, () => {
-	console.log(`Now listening to: http://localhost:${port}`);
-	open(`http://localhost:${port}`);
+	console.log(`Now listening to: http://35.154.130.122:${port}`);
+	open(`http://35.154.130.122:${port}`);
 
 	emit_server_info();
 });
@@ -1691,7 +1691,7 @@ rl.input.on("keypress", async (char, key) => {
 
 	if (key.name === "u") {
 		console.log(`--------------------------`);
-		console.log(`http://localhost:${port}`);
+		console.log(`http://35.154.130.122:${port}`);
 		console.log(`--------------------------`);
 	}
 });
